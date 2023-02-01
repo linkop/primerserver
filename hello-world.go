b599bef90b0e53a056c6902d2e9ab8390a4b8fe8
linkop <guiurbina@gmail.com>
2023-02-01 10:13:54 -0600

    se agrega el proyecto

---
+++ hello-world.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func main() {
+	//routes
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/contact", ContactHandler)
+
+	//start the server
+	http.ListenAndServe(":3000", nil)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hola mundo desde Go"))
+}
+
+func ContactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Pagina de contacto desde Go</h1>"))
+}
